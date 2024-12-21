0815ebae061d875957f5340f059c5cbd51f24831
AlexisCast <castellanos.alexis@hotmail.com>
2024-12-20 23:23:25 -0600

    feat: 3 POO encapsulation

--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -2,9 +2,14 @@
 
 using ObjectOrientedProgramming.Business;
 
-Beer erdingBeer = new Beer("Erdinger", 3);
-var coronaBeer = new Beer("Corona",4);
+Beer erdingBeer = new Beer("Erdinger", 3, -1);
+var coronaBeer = new Beer("Corona", 4, 1);
+var delirium = new ExpiringBeer("Delirium", 4, 8, new DateTime(2024, 12, 01));
 
 Console.WriteLine(erdingBeer.GetInfo());
 Console.WriteLine(erdingBeer.SAlcohol);
+Console.WriteLine(coronaBeer.GetInfo());
+Console.WriteLine(coronaBeer.SAlcohol);
+Console.WriteLine(delirium.GetInfo());
+Console.WriteLine(delirium.SAlcohol);
 
