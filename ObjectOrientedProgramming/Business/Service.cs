b4867f004295fe6c5cd499271cb6b93bec5f2975
AlexisCast <castellanos.alexis@hotmail.com>
2024-12-21 23:19:59 -0600

    feat: 7 POO interfaces

---
+++ ObjectOrientedProgramming/Business/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming.Business
+{
+    public class Service : ISalable
+    {
+        private decimal _amount;
+        private decimal _tax;
+        public Service(decimal amount, decimal tax)
+        {
+            _amount = amount;
+            _tax = tax;
+        }
+
+        public decimal GetPrice()
+            => _amount + _tax;
+    }
+}
