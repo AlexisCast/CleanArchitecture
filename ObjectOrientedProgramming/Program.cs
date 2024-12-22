29fcc1b8599aec6ace94cc3ca4934dbb4f15fe9c
AlexisCast <castellanos.alexis@hotmail.com>
2024-12-21 22:11:55 -0600

    feat: 5 abstract class

--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -2,9 +2,10 @@
 
 using ObjectOrientedProgramming.Business;
 
-Beer erdingBeer = new Beer("Erdinger", 3, -1);
-var coronaBeer = new Beer("Corona", 4, 1);
-var delirium = new ExpiringBeer("Delirium", 4, 8, new DateTime(2024, 12, 01));
+Beer erdingBeer = new Beer("Erdinger", 3, -1, 1009);
+var coronaBeer = new Beer("Corona", 4, 1, 330);
+var delirium = new ExpiringBeer("Delirium", 4, 8, new DateTime(2024, 12, 01), 1000);
+Drink drink = new Beer("Erdinger Black", 5, 5, 500);
 
 Console.WriteLine(erdingBeer.GetInfo());
 Console.WriteLine(erdingBeer.GetInfo("string"));//overloading
@@ -18,4 +19,10 @@ Console.WriteLine(delirium.GetInfo());//overriding
 Console.WriteLine(delirium.GetInfo("string"));//overloading
 Console.WriteLine(delirium.GetInfo(0));//overloading
 Console.WriteLine(delirium.SAlcohol);
+
+//getting from abtract class
+Console.WriteLine(drink.GetQuantity());
+Console.WriteLine(delirium.GetQuantity());
+Console.WriteLine(drink.GetCategory());
+Console.WriteLine(delirium.GetCategory());
 
