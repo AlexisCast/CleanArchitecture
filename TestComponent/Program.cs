0b064e4c990e41066e344b4bd39b25582d24c082
AlexisCast <castellanos.alexis@hotmail.com>
2024-12-23 17:00:26 -0600

    feat: 16 Components basic

---
+++ TestComponent/Program.cs
@@ -0,0 +1,7 @@
+﻿using OperationComponent;
+
+var operations = new Operations();
+
+var result = operations.Some(2, 3); // ../TestComponent/bin/Debug/net8.0 TestComponent.exe uses OperationComponent.dll
+Console.WriteLine(result);
+Console.ReadLine();
