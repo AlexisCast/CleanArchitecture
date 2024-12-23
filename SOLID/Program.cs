87ccc4c343e7bd5d6f2f26ec7cd0d0a3bd022061
AlexisCast <castellanos.alexis@hotmail.com>
2024-12-23 10:59:58 -0600

    feat: 10 SOLID basic

---
+++ SOLID/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+
+
+// NOT SOLID
+//-----------------------------------------------------------
+var beer = new Beer();
+beer.Add("Corona");
+beer.Add("Delirium");
+beer.SaveReport("cervezas.txt");
+
+public class Beer
+{
+    public List<string> _beers;
+
+    public Beer()
+    {
+        _beers = new List<string>();
+    }
+
+    public void Add(string beer)
+        => _beers.Add(beer);
+    public List<string> Get()
+        => _beers;
+    public List<string> GetReport()
+    {
+        var data = new List<string>();
+        foreach (var beer in _beers)
+        {
+            data.Add("Cerveza: " + beer);
+        }
+        return data;
+    }
+    public void SaveReport(string filePath)
+    {
+        using (var writer = new StreamWriter(filePath))
+        {
+            foreach (var beer in GetReport())
+            {
+                writer.WriteLine(beer);
+            }
+        }
+    }
+}
+
