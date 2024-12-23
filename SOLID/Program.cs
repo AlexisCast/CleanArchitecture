dc7f7d1402202977de1e0a383123c8e5a269476d
AlexisCast <castellanos.alexis@hotmail.com>
2024-12-23 11:16:36 -0600

    feat: 11 SOLID Single Responsability

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,11 +1,72 @@
 ﻿
+var beerData = new BeerData();
+beerData.Add("Corona");
+beerData.Add("Delirium");
+
+var reportGeneratorBeer = new ReportGeneratorBeer(beerData);
+
+var report = new Report();
+
+var data = reportGeneratorBeer.Generate();
+
+report.Save(data, "cervezas.txt");
+
+public class BeerData
+{
+    public List<string> _beers;
+
+    public BeerData()
+    {
+        _beers = new List<string>();
+    }
+
+    public void Add(string beer)
+        => _beers.Add(beer);
+    public List<string> Get()
+        => _beers;
+}
+
+public class ReportGeneratorBeer
+{
+    private BeerData _beerData;
+    public ReportGeneratorBeer(BeerData beerData)
+    {
+        _beerData = beerData;
+    }
+
+    public List<string> Generate()
+    {
+        var data = new List<string>();
+        int i = 1;
+        foreach (var beer in _beerData.Get())
+        {
+            data.Add(i + " Cerveza: " + beer);
+            i++;
+        }
+        return data;
+    }
+}
+
+public class Report
+{
+    public void Save(List<string> data, string filePath)
+    {
+        using (var writer = new StreamWriter(filePath))
+        {
+            foreach (var beer in data)
+            {
+                writer.WriteLine(beer);
+            }
+        }
+    }
+}
 
 
 
 
 // NOT SOLID
 //-----------------------------------------------------------
-var beer = new Beer();
+/*var beer = new Beer();
 beer.Add("Corona");
 beer.Add("Delirium");
 beer.SaveReport("cervezas.txt");
@@ -42,5 +103,5 @@ public class Beer
             }
         }
     }
-}
+}*/
 
