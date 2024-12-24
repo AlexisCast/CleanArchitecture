ed7ece87e8a03aad54bd9e429309762a9541c06f
AlexisCast <castellanos.alexis@hotmail.com>
2024-12-23 18:05:11 -0600

    feat: 20 Components ADP

--- TestComponent/Program.cs
+++ TestComponent/Program.cs
@@ -12,3 +12,75 @@ Console.ReadLine();
 var beer = new Beers();
 
 // CRP talks about not depending on things that will not be used... related to interface segregation principle
+
+
+
+
+
+// IS in complaince with ADP
+public class C
+{
+    private readonly IA _a;
+    public C(IA a)
+    {
+        _a = a;
+    }
+}
+
+public interface IA
+{
+    public void MethodA();
+}
+
+public class A : IA
+{
+    private readonly B _b;
+    public A(B b)
+    {
+        _b = b;
+    }
+    public void MethodA()
+    {
+
+
+    }
+}
+
+public class B
+{
+    private readonly C _c;
+    public B(C c)
+    {
+
+        _c = c;
+    }
+}
+
+// NOT in complaince with ADP
+/*public class C
+{
+    private readonly A _a;
+    public C(A a)
+    {
+        _a = a;
+    }
+}
+
+public class A
+{
+    private readonly B _b;
+    public A(B b)
+    {
+        _b = b;
+    }
+}
+
+public class B
+{
+    private readonly C _c;
+    public B(C c)
+    {
+
+        _c = c;
+    }
+}*/
