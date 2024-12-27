78da41567e4f548129c8b623b4b8b25e8b4e420a
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-12-27 04:21:35 -0600

    feat: 31Clean Architecture -> Dependency Injection in API - Frameworks and Drivers(67)

--- CA-FrameworkDrivers-API/Program.cs
+++ CA-FrameworkDrivers-API/Program.cs
@@ -1,3 +1,8 @@
+using CA_ApplicationLayer;
+using CA_InterfaceAdapter_Data;
+using CA_InterfaceAdapters_Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//dependencies
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped<GetBeerUseCase>();
 
 var app = builder.Build();
 
@@ -22,9 +35,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("/beer", () =>
+//app.MapGet("/beer", () =>
+//{
+//    return "Hello World!";
+//})
+//.WithName("beers")
+//.WithOpenApi();
+
+app.MapGet("/beer", async (GetBeerUseCase beerUseCase) =>
 {
-    return "Hello World!";
+    return await beerUseCase.ExecuteAsync();
 })
 .WithName("beers")
 .WithOpenApi();
