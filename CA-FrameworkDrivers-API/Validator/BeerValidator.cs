0adbe87a2c8de7a9f328aaf1efe120572f2f7b3c
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-12-29 14:07:26 -0600

    feat: 40 Deep Dive Validations in Framework

---
+++ CA-FrameworkDrivers-API/Validator/BeerValidator.cs
@@ -0,0 +1,15 @@
+﻿using CA_InterfaceAdapters_Mappers.DTOs.Requests;
+using FluentValidation;
+
+namespace CA_FrameworkDrivers_API.Validator
+{
+    public class BeerValidator : AbstractValidator<BeerRequestDTO>
+    {
+        public BeerValidator()
+        {
+            RuleFor(dto => dto.Name).NotEmpty().WithMessage("The beer needs to have a name.");
+            RuleFor(dto => dto.Style).NotEmpty().WithMessage("The beer needs to have a style.");
+            RuleFor(dto => dto.Alcohol).GreaterThan(0).NotEmpty().WithMessage("The beer needs to have alcohol.");
+        }
+    }
+}
