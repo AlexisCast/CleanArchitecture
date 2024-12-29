1c4e4b7c7c378ffbee22ae6ef60c5ebec4e37f60
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-12-28 19:40:34 -0600

    feat: 39 Deep Dive Middleware Exceptions

---
+++ CA-FrameworkDrivers-API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Text.Json;
+using CA_ApplicationLayer.Exceptions;
+
+namespace CA_FrameworkDrivers_API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ValidationException ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        private static async Task HandleExceptionAsync(HttpContext context,
+            ValidationException exception)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            var statusCode = HttpStatusCode.InternalServerError;
+            var result = JsonSerializer.Serialize(new
+            {
+                error = exception.Message,
+                detail = exception.InnerException?.Message
+            });
+
+            response.StatusCode = (int)statusCode;
+            await response.WriteAsync(result);
+        }
+    }
+
+}
