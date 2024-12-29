081b19f56840293f6f25296ff851001e1181d0f9
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-12-29 14:31:10 -0600

    feat: 41 Deep Dive Presentation Logic(new model and presenter)

---
+++ CA-InterfaceAdapters-Presenters/BeerDetailPresenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CA_ApplicationLayer;
+using CA_EnterpriseLayer;
+
+namespace CA_InterfaceAdapters_Presenters
+{
+    public class BeerDetailPresenter : IPresenter<Beer, BeerDetailViewModel>
+    {
+        public IEnumerable<BeerDetailViewModel> Present(IEnumerable<Beer> beers)
+        {
+            return beers.Select(b => new BeerDetailViewModel //transformation
+            {
+                Id = b.Id,
+                Name = b.Name,
+                Alcohol = b.Alcohol + "%",
+                Color = b.IsStrongBeer() ? "red" : "green",
+                Style = b.Style,
+                Message = b.IsStrongBeer() ? "Strong Beer" : "",
+            });
+        }
+    }
+
+}
