c9588c0d4783743160a435aa5755cc6ac483b47f
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-12-30 11:57:35 -0600

    feat: 43 Deep Dive Adapter for third party service

---
+++ CA-InterfaceAdapter-Adapters/PostExternalServiceAdapter.cs
@@ -0,0 +1,26 @@
+﻿using CA_ApplicationLayer;
+using CA_EnterpriseLayer;
+using CA_InterfaceAdapter_Adapters.DTOs;
+
+namespace CA_InterfaceAdapter_Adapters
+{
+    public class PostExternalServiceAdapter : IExternalServiceAdapter<Post>
+    {
+        private readonly IExternalService<PostServiceDTO> _externalService;
+        public PostExternalServiceAdapter(IExternalService<PostServiceDTO> externalService)
+        {
+            _externalService = externalService;
+        }
+        public async Task<IEnumerable<Post>> GetDataAsync()
+        {
+            var postES = await _externalService.GetContentAsync();
+            var post = postES.Select(p => new Post //transformation
+            {
+                Id = p.Id,
+                Title = p.Title,
+                Body = p.Body
+            });
+            return post;
+        }
+    }
+}
